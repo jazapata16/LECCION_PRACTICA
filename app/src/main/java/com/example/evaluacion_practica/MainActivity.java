b93f0c87535e4874826fb91ff19c43f518872949
lllll <lllll>
2023-06-15 17:25:50 -0500

    ultimo

--- app/src/main/java/com/example/evaluacion_practica/MainActivity.java
+++ app/src/main/java/com/example/evaluacion_practica/MainActivity.java
@@ -4,24 +4,35 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private EditText txt_busqueda;
+    private Button btn_busqueda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_busqueda = findViewById(R.id.button2);
+        btn_busqueda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buscarheroe(v);
+            }
+        });
     }
 
     public void buscarheroe(View view){
+        System.out.println("prueba");
         txt_busqueda= findViewById(R.id.txt_busqueda);
 
         String texto = txt_busqueda.getText().toString();
 
         Intent intent = new Intent(this,consulta.class);
         intent.putExtra("txt_busqueda", texto);
+        startActivity(intent);
     }
 
 
--- app/src/main/java/com/example/evaluacion_practica/consulta.java
+++ app/src/main/java/com/example/evaluacion_practica/consulta.java
@@ -4,15 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class consulta extends AppCompatActivity {
     private String txt_busqueda;
@@ -25,14 +29,15 @@ public class consulta extends AppCompatActivity {
         setContentView(R.layout.activity_consulta);
 
         Intent intent = getIntent();
-
-        txt_busqueda = intent.getStringExtra("txt_busqueda");
+        this.txt_busqueda = (String)intent.getExtras().get("txt_busqueda");
         busqueda(txt_busqueda);
     }
 
     public void busqueda(String txt_busqueda){
+        System.out.println(txt_busqueda);
 
-        String url_temp = "https://superheroapi.com/api/"+token+"/search/"+txt_busqueda;
+        String url_temp = "https://superheroapi.com/api/925571252058726/search/"+txt_busqueda;
+        System.out.println(url_temp);
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.GET, url_temp, null,
                 new Response.Listener<JSONObject>() {
@@ -40,11 +45,7 @@ public class consulta extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         System.out.println(response);
                         try {
-
-                            System.out.println(response.getString("results"));
-
-
-
+                           System.out.println(response.getString("results"));
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -53,7 +54,8 @@ public class consulta extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
             }
-        });;
+        });
+        System.out.println(request);
         mQueue.add(request);
 
     }
