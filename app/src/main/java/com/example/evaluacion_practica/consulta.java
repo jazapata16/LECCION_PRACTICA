eeea726e45a5fa92f581a4e127887734b5de1168
lllll <lllll>
2023-06-15 16:34:56 -0500

    get

--- app/src/main/java/com/example/evaluacion_practica/consulta.java
+++ app/src/main/java/com/example/evaluacion_practica/consulta.java
@@ -4,8 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
 public class consulta extends AppCompatActivity {
     private String txt_busqueda;
+
+    private String token = "925571252058726";
+    private RequestQueue mQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +25,32 @@ public class consulta extends AppCompatActivity {
         Intent intent = getIntent();
 
         txt_busqueda = intent.getStringExtra("txt_busqueda");
+        busqueda(txt_busqueda);
+    }
+
+    public void busqueda(String txt_busqueda){
+
+        String url_temp = "https://superheroapi.com/api/"+token+"/search/"+txt_busqueda;
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET, url_temp, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response);
+                        try {
+                            tempValue.setText(response.getString("temperatura")+ " °C");
+                            response.getString("results");
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });;
+        mQueue.add(request);
+
     }
 }
