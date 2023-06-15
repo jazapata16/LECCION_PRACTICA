69a9fef4d79aba1185ff21c10b0640d5f0add9a9
lllll <lllll>
2023-06-15 16:09:34 -0500

    commit ultimo

--- app/src/main/java/com/example/evaluacion_practica/MainActivity.java
+++ app/src/main/java/com/example/evaluacion_practica/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.evaluacion_practica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText txt_busqueda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buscarheroe(View view){
+        txt_busqueda= findViewById(R.id.txt_busqueda);
+
+        String texto = txt_busqueda.getText().toString();
+
+        Intent intent = new Intent(this,consulta.class);
+        intent.putExtra("txt_busqueda", texto);
     }
 
 
--- app/src/main/java/com/example/evaluacion_practica/consulta.java
+++ app/src/main/java/com/example/evaluacion_practica/consulta.java
@@ -1,13 +1,18 @@
 package com.example.evaluacion_practica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class consulta extends AppCompatActivity {
-
+    private String txt_busqueda;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta);
+
+        Intent intent = getIntent();
+
+        txt_busqueda = intent.getStringExtra("txt_busqueda");
     }
 }
